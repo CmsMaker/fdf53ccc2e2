a10fee86a9af5d8993df7cf27737d376a7cf7608
Cms Maker <mohamademiri@gmail.com>
2016-04-07 02:37:30 +0430

    Change Config-File
    Adding Sms Api

--- config.php
+++ config.php
@@ -12,6 +12,13 @@ $request = "200000";
 $refering_activation = "50000";
 $level_silver_cost = "50000";
 $level_golden_cost = "70000";
-$payline = "کد پی لاین";
+$payline = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
 $link = "http://basdid.tk/";
+
+
+
+$smsPanleAPI  = "30006320700000";
+$smsPanelPass = "741000";
+
+
 ?>
