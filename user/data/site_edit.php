fd60f386897f1cb87a2f88073152aa2c86901b5e
Cms Maker <mohamademiri@gmail.com>
2016-04-08 22:50:01 +0430

    sms Send On site Edit

--- user/data/site_edit.php
+++ user/data/site_edit.php
@@ -6,9 +6,15 @@ include "../../include/mysql.class.php";
 $db = new Mysql($host,$dbuname,$dbpass,$dbname,false);
 $db->Database_Connect();
 
-$site_id = $_POST['site_id'];
-$title = $_POST['title'];
-$url = $_POST['url'];
+	$funcPath = 
+		$_SERVER[ 'DOCUMENT_ROOT' ] . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR . "func.php";
+	require_once( $funcPath );
+	
+	$mysqli = getMySqlHandel();
+
+$site_id =  $mysqli->real_escape_string( $_POST['site_id'] );
+$title =     $mysqli->real_escape_string( $_POST['title'] );
+$url =     $mysqli->real_escape_string( $_POST['url'] );
 
 if($title == ""){
 	echo "<div class='message-error'>نام سایت را وارد نمائید.</div>";
@@ -17,6 +23,7 @@ elseif($url == "" || $url == "http://www."){
 	echo "<div class='message-error'>آدرس سایت را وارد نمائید.</div>";
 }
 elseif($db->sql_getrows("SELECT * FROM `sites` WHERE `id`='".$site_id."'") > 0){
+	sendSmsToUser( 5 );
 	$db->sql_query("UPDATE `sites` SET `title`='$title',`url`='$url',`status`='0' WHERE `id`='$site_id' AND `username`='".$_SESSION['user']."'");
 		echo "<div class='message-success'>سایت ویرایش شد و منتظر تایید مدیریت می باشد.</div>";
 	} else {
