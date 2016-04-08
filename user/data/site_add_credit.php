9daac0b8db2f9571f95183ac09fe7898eb809fbd
Cms Maker <mohamademiri@gmail.com>
2016-04-08 22:47:07 +0430

    sms on add credit to site

--- user/data/site_add_credit.php
+++ user/data/site_add_credit.php
@@ -6,8 +6,15 @@ include "../../include/mysql.class.php";
 $db = new Mysql($host,$dbuname,$dbpass,$dbname,false);
 $db->Database_Connect();
 
-$site_id = $_POST['site_id'];
-$credit = $_POST['credit'];
+	
+	$funcPath = 
+		$_SERVER[ 'DOCUMENT_ROOT' ] . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR . "func.php";
+	require_once( $funcPath );
+	
+	$mysqli = getMySqlHandel();
+
+$site_id = $mysqli->real_escape_string( $_POST['site_id'] );
+$credit =  $mysqli->real_escape_string( $_POST['credit']  );
 
 $db->sql_query("SELECT * FROM `user` WHERE `username`='".$_SESSION['user']."'");
 $fetch = $db->sql_fetcharray();
@@ -25,6 +32,9 @@ elseif($credit > $fetch['credit']) {
 elseif($credit == 0 || $credit < 0) {
 	echo "<div class='message-error'>تعداد بازدید نامعتبر است.</div>";
 } else {
+	
+	sendSmsToUser( 5 );
+	
 	$db->sql_query("UPDATE `user` SET `credit`=credit-$credit WHERE `username`='".$_SESSION['user']."'");
 	$db->sql_query("UPDATE `sites` SET `credit`=credit+$credit WHERE `id`='".$site_id."'"); {
 		echo "<div class='message-success'>بازدید افزوده شد.</div>";
